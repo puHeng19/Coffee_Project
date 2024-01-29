b01b78d30c57e2e8660049f5805d5ed0cc0f0d7f
puHeng19 <158069514+puHeng19@users.noreply.github.com>
2024-01-29 22:44:37 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slide", {
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
